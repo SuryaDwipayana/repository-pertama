3eefd68fcc238eb7f9242c4eeded0788d30deafa
Surya Dwipayana <suryadwipayana1@gmail.com>
2017-06-14 20:54:37 +0800

    upload Dashboard Activity

---
+++ app/src/main/java/com/example/suryadwipayana/miwok/DashboardActivity.java
@@ -0,0 +1,27 @@
+package com.example.suryadwipayana.miwok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Surya Dwipayana on 6/14/2017.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
--- app/src/main/java/com/example/suryadwipayana/miwok/MainActivity.java
+++ app/src/main/java/com/example/suryadwipayana/miwok/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.suryadwipayana.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Lifecycle" , "onCreate");
+
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentdashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(intentdashboard);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("Lifecycle" , "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle" , "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle" , "onDestroy");
     }
 }
